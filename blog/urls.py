3ffba14747f0d569d022bddbc9e1daabf95ecde9
Arutoru <arthurtankwa@gmail.com>
2022-12-05 14:06:24 +0000

    Staticfiles debug 4

--- blog/urls.py
+++ blog/urls.py
@@ -16,4 +16,4 @@ urlpatterns = [
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
