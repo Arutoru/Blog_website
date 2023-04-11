5860a3fc2b9e27b0a8607cf34f0ca245cbd7027e
Arutoru <arthurtankwa@gmail.com>
2023-04-11 16:02:11 +0100

    test GDAL

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+GDAL_LIBRARY_PATH = os.path.join(BASE_DIR,'GDAL/gdal304')
+GEOS_LIBRARY_PATH = os.path.join(BASE_DIR,'GDAL/geos_c')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
